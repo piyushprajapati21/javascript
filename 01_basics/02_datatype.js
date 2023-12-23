01ac154d6ed760ea8bf20c35dd409b29026785d6
piyushprajapati21 <118489957+piyushprajapati21@users.noreply.github.com>
2023-12-23 02:37:48 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict" ; // treat all JS code as newer version
+
+//alert('hello - JS'); // alert not working on Node JS
+console.log(3+3);
+
+let name = 'Piyush'
+let age = '35';
+let isLogin = true
+
+// number = 2 to power 53
+// bigint = 
+// boolean = true or false
+// null = standalone value
+// undefined = value is not assigned
+// string = string value
+// symbol = for unique
+
+//object 
+
+console.log(typeof "122")
+console.log(typeof 122)
