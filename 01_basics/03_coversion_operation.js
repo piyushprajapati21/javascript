1938670bf698e9552fb3eb17f9eaf7a49f62675b
piyushprajapati21 <118489957+piyushprajapati21@users.noreply.github.com>
2023-12-23 03:26:43 +0000

    conversion

--- 01_basics/03_coversion_operation.js
+++ 01_basics/03_coversion_operation.js
@@ -8,3 +8,10 @@ let isLogin = 1;   //1-> true and 0->false .,, .. ""-> false
 let isboolean = Boolean();
 console.log(isboolean); 
 
+/**** operations */
+
+let val = 4
+let negval = -val
+console.log(negval)
+
+console.log(negval+10)
