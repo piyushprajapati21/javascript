69e04674df1427f5be242fd26ec2f39cf3afcf3f
piyushprajapati21 <118489957+piyushprajapati21@users.noreply.github.com>
2023-12-23 03:25:38 +0000

    conversion operations

---
+++ 01_basics/03_coversion_operation.js
@@ -0,0 +1,10 @@
+let score = '33';
+console.log(typeof score)
+
+let valueNumber = Number(score);
+console.log(typeof valueNumber)
+
+let isLogin = 1;   //1-> true and 0->false .,, .. ""-> false
+let isboolean = Boolean();
+console.log(isboolean); 
+
