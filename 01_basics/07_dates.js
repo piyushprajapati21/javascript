7a76d0b400b520d46709fca49f4cf393ebfaad15
piyushprajapati21 <118489957+piyushprajapati21@users.noreply.github.com>
2023-12-23 22:43:20 +0000

    array

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,17 @@
+const date = new Date()
+console.log(date.toString())
+
+console.log(date.toLocaleString())
+console.log(date.toDateString())
+
+////let newdate = new date(2023, 12, 21)
+//console.log(newdate.toLocaleString())
+
+
+/* let myTimestamp = Date.now();
+console.log(myTimestamp)
+console.log(date.getTime()) */
+
+let newdate = new Date()
+console.log(newdate.getDate());
+console.log(newdate.getMonth());
---
+++ 02_basics/01_array.js
@@ -0,0 +1,14 @@
+//Array
+
+const myArray = [1,2,3,4,5,6,7];
+console.table(myArray)
+
+const newArr = myArray.join()
+console.log(newArr)
+
+//slice
+const arr1 = myArray.slice(1,3)
+console.log(arr1)
+
+const arr2 = myArray.splice(1,3)
+console.log(arr2)
