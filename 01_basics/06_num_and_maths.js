6effc624b93adc7075cfba928f144fdca03a3dc2
piyushprajapati21 <118489957+piyushprajapati21@users.noreply.github.com>
2023-12-23 18:11:09 +0000

    math and number

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,12 @@
+const score = 2345
+
+const balance = new Number(2000)
+console.log(balance)
+
+console.log(balance.toFixed(4))
+
+const anobalance = new Number(345.100);
+console.log(anobalance.toPrecision(3))
+
+
+//abs. round, ceil, floor, min , max, random
