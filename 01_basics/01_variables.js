2fd19a5a026ba16eca858bca3fe0e3e34194cf2e
piyushprajapati21 <118489957+piyushprajapati21@users.noreply.github.com>
2023-12-23 02:19:07 +0000

    Variable added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 15555;
+let accountEmail = 'ppp@pp.com';
+var accountPwd = '12345';
+accountCity = 'Ahmedabad';
+let accountState;
+//accountId = 222; //not allowed due to contant variable
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPwd,accountCity,accountState]);
+
+/*
+Prefer not to use var because scope and functional scope
+*/
