f6522ef50699debff2c69767b24086dfb9edf8ab
piyushprajapati21 <118489957+piyushprajapati21@users.noreply.github.com>
2023-12-24 15:59:02 +0000

    object de construct

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,16 @@
+const course = {
+    'name' : 'javascript',
+    'price' : 999,
+    'tutor' : 'Piyush'
+}
+
+console.log(course.tutor)
+
+const {tutor} = course
+console.log(tutor)
+
+const {tutor:t} = course. // object deconstruct 
+console.log(t)
+
+/** API Json */
+
